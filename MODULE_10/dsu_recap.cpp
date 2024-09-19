b6481b199b5a5a73348012df9ca5db0d3841ba44
roni <105470281+Ronibaidhya@users.noreply.github.com>
2024-09-19 09:27:29 +0600

    Update dsu_recap.cpp

--- MODULE_10/dsu_recap.cpp
+++ MODULE_10/dsu_recap.cpp
@@ -8,8 +8,10 @@ void dsu_initialize(int n){
     for(int i=0;i<n;i++){
         par[i] = -1;
     }
-    par[1] = 3;
-    par[2] = 1;
+    par[1] = 2;
+    par[2] = 3;
+    par[5] = 6;
+    par[6] = 7;
 }
 
 //O(n)
@@ -19,22 +21,25 @@ void dsu_initialize(int n){
 //     }
 //     return find(par[node]);
 // }
-int find(int node){
+int dsu_find(int node){
     if(par[node]==-1){
         return node;
     }
-    int leader = find(par[node]);
+    int leader = dsu_find(par[node]);
     par[node] = leader;
     return leader; 
 }
 
+void dsu_union(int node1, int node2){
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    par[leaderA] = leaderB;
+}
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
-    dsu_initialize(4);
-    cout<<find(0)<<endl;
-    cout<<find(1)<<endl;
-    cout<<find(2)<<endl;
-    cout<<find(3)<<endl;
+    dsu_initialize(8);
+    dsu_union(1,5);
+    cout<<dsu_find(1)<<endl;
     return 0;
 }
